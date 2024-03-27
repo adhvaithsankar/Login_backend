7cf4e7ec9f1b554fcd470a3e5be8652a00336584
Adhvaith Sankar S S <57494130+adhvaithsankar@users.noreply.github.com>
2024-03-27 20:50:40 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-DB_URI = "postgres://aws_invoice_user:oFsKPV03cSTIvRFwmkEiiJhnc99dNhxp@dpg-cnu1hgda73kc73f5966g-a.singapore-postgres.render.com/aws_invoice"
+DB_URI = "postgresql://aws_invoice_user:oFsKPV03cSTIvRFwmkEiiJhnc99dNhxp@dpg-cnu1hgda73kc73f5966g-a.singapore-postgres.render.com/aws_invoice"
 
 engine = _sql.create_engine(DB_URI)
 
